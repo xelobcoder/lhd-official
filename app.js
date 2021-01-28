81bafb84058a942a17b6d46e37f668bbf24dc939
Tiifu Hamza <hamzatiifu45@gmail.com>
2021-01-27 23:29:15 -0800

    changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require ("express");
 const app = express();
 const port = process.env.PORT || 4000;
 const ejs = require ("ejs");
-const database = require ("./models/database/db");
+// const database = require ("./models/database/db");
 
 
 app.use(express.static("public"))
--- models/database/db.js
+++ models/database/db.js
@@ -1,33 +1,33 @@
-const mysql = require ("mysql");
+// const mysql = require ("mysql");
 
-const database = mysql.createConnection(
-    {
-        host: "localhost",
-        user: "root",
-        database: "labhill_commission",
-        password: ""
-    }
-)
+// const database = mysql.createConnection(
+//     {
+//         host: "localhost",
+//         user: "root",
+//         database: "labhill_commission",
+//         password: ""
+//     }
+// )
 
-database.connect(function(err){
-    if(err) throw err;
-    console.log(`connection to database successful`);
-})
+// database.connect(function(err){
+//     if(err) throw err;
+//     console.log(`connection to database successful`);
+// })
 
 
 
-database.end(function(err){
-   if(err) {
-    return (
-        {
-            err_code: err.code,
-            err_sql:err.sql,
-            err_message:err.message,
-            err_:err.message,
-            err_stack: err.stack
-        }
-    )
-   }
-})
+// database.end(function(err){
+//    if(err) {
+//     return (
+//         {
+//             err_code: err.code,
+//             err_sql:err.sql,
+//             err_message:err.message,
+//             err_:err.message,
+//             err_stack: err.stack
+//         }
+//     )
+//    }
+// })
 
-module.exports = database;
+// module.exports = database;
